7e849c2cccc4cb2efb5b4d6a7c3fbb2d9dec4b09
soofyaa <mazaeva1sofya@gmail.com>
2022-12-06 11:51:16 +0300

    kr 2

--- 8/Main2.java
+++ 8/Main2.java
@@ -5,7 +5,7 @@ public class Main2 {
         Scanner scanner = new Scanner(System.in);
         CasualCar casualCar = new CasualCar(1000, 450, 100, 150);
         F1Car f1Car = new F1Car(1000, 450, 60, 220);
-        BoostedCar boostedCar = new BoostedCar(1000,450,80, 200, 1);
+        BoostedCar boostedCar = new BoostedCar(1000,450,80, 200, 3);
         System.out.println(casualCar);
         System.out.println(f1Car);
         System.out.println(boostedCar);
