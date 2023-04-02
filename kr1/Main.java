2234215ffb5c9f347bf659b2f371ed896ee41622
sofya <mazaeva1sofya@gmail.com>
2023-04-02 04:25:47 +0300

    add hw6

--- kr1/Main.java
+++ kr1/Main.java
@@ -25,6 +25,7 @@ public class Main {
             @Override
             public int compare(Car o1, Car o2) {
                 return Integer.compare(o1.getKms(), o2.getKms());
+                //return o1.getKms().compareTo(o2.getKms());
             }
         };
 
